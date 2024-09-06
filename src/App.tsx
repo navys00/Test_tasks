c0d27986954d0c65cfea91853eaa0b38481efea0
navys00 <73278416+navys00@users.noreply.github.com>
2024-09-06 20:48:06 +0300

    add app1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { requestUsers, requestUsersWithError, User, Query } from "./api";
-import "./styles.css";
-import { Index2 } from "./index2";
-import Requirements from "./Requirements"
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Success } from './pages/Success';
 
-// Примеры вызова функций, в консоли можно увидеть возвращаемые результаты
-requestUsers({ name: "", age: "", limit: 4, offset: 0 }).then(console.log);
-requestUsersWithError({ name: "", age: "", limit: 4, offset: 0 }).catch(
-  console.error
-);
+const App = () => {
 
-export default function App() {
-  return <Index2 />;
-  // return <Requirements />
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Success' element={<Success />} />
+      </Routes>
+    </div>
+  );
 }
+
+export default App
